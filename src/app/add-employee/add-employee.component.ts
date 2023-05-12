3aa3adaaa58a5fcac0ac5adcce682b6028972cda
ashwitashetty <ashwitha.shetty@robosoftin.com>
2023-05-12 10:59:31 +0530

    material model

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
-  styleUrls: ['./add-employee.component.css']
+  styleUrls: ['./add-employee.component.css'],
 })
-export class AddEmployeeComponent {
+export class AddEmployeeComponent implements OnInit {
+  addEmpForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.addEmpForm=new FormGroup({
+      name:new FormControl('',Validators.required),
+      designation:new FormControl(''),
+      joiningdate:new FormControl(''),
+      technologies:new FormControl('')
+    })
+  }
+  addEmployee(){
+    console.log("employee save clicked");
+    console.log(this.addEmpForm)
+  }
 }
